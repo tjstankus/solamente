e3547e59e37ead3a8c457c667095b596384c9de0
TJ Stankus <tjstankus@gmail.com>
2018-05-09 21:02:06 -0400

    Cycle todo keyword

--- lib/solamente/heading.rb
+++ lib/solamente/heading.rb
@@ -1,13 +1,34 @@
 module Solamente
   class Heading
-    attr_reader :line
+    attr_reader :line, :todo_keywords
 
-    def initialize(line)
+    def initialize(line, todo_keywords)
       @line = line
+      @todo_keywords = todo_keywords
     end
 
-    def cycle_todo_state(states)
-      line.gsub("* ", "* #{states.first} ")
+    def cycle_todo_keyword
+      [ match_line[:stars],
+        next_todo_keyword(match_line[:todo_keyword]),
+        match_line[:heading_text]].reject(&:empty?).join(" ")
+    end
+
+    def next_todo_keyword(current_keyword)
+      wrapped_keywords[wrapped_keywords.index(current_keyword.to_s) + 1]
+    end
+
+    def match_line
+      @match ||= line.match(%r{
+        # beginning of line, some number of stars, space
+        (?<stars>^\*+)\040
+        # optional capture group for keyword
+        ?(?<todo_keyword>#{todo_keywords.join('|')})?\040
+        # rest of line is heading text
+        (?<heading_text>.*)}x)
+    end
+
+    def wrapped_keywords
+      @wrapped_keywords ||= Array.new(todo_keywords).prepend("").append("")
     end
   end
 end
--- spec/solamente/heading_spec.rb
+++ spec/solamente/heading_spec.rb
@@ -1,9 +1,23 @@
 require "spec_helper"
 
 RSpec.describe Solamente::Heading do
-  it "cycles todo state" do
-    heading = described_class.new("* Example heading")
-    cycled_heading = heading.cycle_todo_state(["TODO"])
-    expect(cycled_heading).to eq("* TODO Example heading")
+  describe "#cycle_todo_keyword" do
+    it "cycles todo keyword from empty" do
+      heading = described_class.new("* Example heading", ["TODO"])
+      cycled_heading = heading.cycle_todo_keyword
+      expect(cycled_heading).to eq("* TODO Example heading")
+    end
+
+    it "cycles to next keyword" do
+      heading = described_class.new("* TODO Example heading", ["TODO", "WIP"])
+      cycled_heading = heading.cycle_todo_keyword
+      expect(cycled_heading).to eq("* WIP Example heading")
+    end
+
+    it "cycles to empty from last keyword" do
+      heading = described_class.new("* DONE Example heading", ["TODO", "DONE"])
+      cycled_heading = heading.cycle_todo_keyword
+      expect(cycled_heading).to eq("* Example heading")
+    end
   end
 end
